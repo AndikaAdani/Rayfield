f78dc550c8a21d749a3de184b5599e8d8b79e0e6
Kcidna <139174641+AndikaAdani@users.noreply.github.com>
2024-05-04 12:28:00 +0700

    Update Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -114,7 +114,7 @@ local Players = game:GetService("Players")
 local CoreGui = game:GetService("CoreGui")
 
 -- Interface Management
-local Rayfield = game:GetObjects("rbxassetid://17376965065")[1]
+local Rayfield = game:GetObjects("rbxassetid://17377058295")[1]
 
 Rayfield.Enabled = false
 
